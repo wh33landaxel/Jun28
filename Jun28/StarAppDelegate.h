d7388de7b23728f7d971837f7fda2d3795753d1f
wh33landaxel <xlnunez317@gmail.com>
2012-06-28 14:55:10 -0400

    Initial Commit

---
+++ Jun28/StarAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StarAppDelegate.h
+//  Jun28
+//
+//  Created by Axel Nunez on 6/28/12.
+//  Copyright (c) 2012 CUNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StarAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
